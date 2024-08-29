1480f36b0076f68a8c450bcfdaec2088e5738596
SeoHee3478 <seohee3478@gmail.com>
2024-08-29 22:15:39 +0900

    Feat: home 페이지와 posts 페이지 분리

--- seoya-log/src/app/page.tsx
+++ seoya-log/src/app/page.tsx
@@ -1,15 +1,7 @@
-import PostCardItem from "@/components/block/PostCardItem";
 export default function Home() {
-  // 더미 데이터
-  const category = "Tech";
-  const title = "Exploring Next.js 13";
-  const content =
-    "Next.js 13 introduces several new features, including the new App Directory, Turbopack, and more. Let's dive into these new features and see how they can enhance your development workflow.";
-
   return (
     <div>
       <h1>메인 페이지!</h1>
-      <PostCardItem category={category} title={title} content={content} />
     </div>
   );
 }
--- seoya-log/src/app/posts/page.tsx
+++ seoya-log/src/app/posts/page.tsx
@@ -1,5 +1,18 @@
+import PostCardItem from "@/components/block/PostCardItem";
+
 const PostListPage = () => {
-  return <h1>게시물 리스트 페이지 !</h1>;
+  // 더미 데이터
+  const category = "Tech";
+  const title = "Exploring Next.js 13";
+  const content =
+    "Next.js 13 introduces several new features, including the new App Directory, Turbopack, and more. Let's dive into these new features and see how they can enhance your development workflow.";
+
+  return (
+    <div>
+      <h1>게시물 리스트 페이지 !</h1>
+      <PostCardItem category={category} title={title} content={content} />
+    </div>
+  );
 };
 
 export default PostListPage;
