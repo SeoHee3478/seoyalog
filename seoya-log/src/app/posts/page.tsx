1e0370a0a3960f40a77be822a359bae4424180f4
SeoHee3478 <seohee3478@gmail.com>
2024-10-29 23:11:13 +0900

    Feat: 블로그 post 리스트 페이지 구현

--- seoya-log/src/app/posts/page.tsx
+++ seoya-log/src/app/posts/page.tsx
@@ -1,5 +1,20 @@
-const PostListPage = () => {
-  return <h1>게시물 리스트 페이지 !</h1>;
-};
+// pages/posts/page.tsx
+import Link from "next/link";
+import { dummyPosts } from "@/helper/lib/dummyData"; // 더미 데이터 사용
 
-export default PostListPage;
+export default function PostsPage() {
+  return (
+    <div>
+      <h1>All Posts</h1>
+      <ul>
+        {dummyPosts.map((post) => (
+          <li key={post.id}>
+            <Link href={`/posts/${post.category}/${post.id}`}>
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
