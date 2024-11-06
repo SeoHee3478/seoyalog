4a587d8127e15a8ca6d5885d5b4da111595e801e
SeoHee3478 <seohee3478@gmail.com>
2024-11-06 22:49:26 +0900

    Feat: 리스트 페이지 md파일에서 불러온 title, date 데이터 보여주기

--- seoya-log/src/app/posts/page.tsx
+++ seoya-log/src/app/posts/page.tsx
@@ -1,16 +1,43 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 import Link from "next/link";
-import { dummyPosts } from "@/helper/lib/dummyData";
 
-export default function PostsPage() {
+async function getPosts() {
+  const postsDirectory = path.join(process.cwd(), "content");
+  const filenames = fs.readdirSync(postsDirectory);
+
+  const posts = filenames.map((filename) => {
+    const filePath = path.join(postsDirectory, filename);
+    const fileContents = fs.readFileSync(filePath, "utf8");
+    const { data } = matter(fileContents);
+
+    return {
+      id: filename.replace(/\.md$/, ""),
+      title: data.title,
+      category: data.category,
+      date: data.date,
+      ...data,
+    };
+  });
+
+  return posts;
+}
+
+export default async function PostsPage() {
+  const posts = await getPosts();
+
   return (
     <div>
       <h1>All Posts</h1>
       <ul>
-        {dummyPosts.map((post) => (
+        {posts.map((post) => (
           <li key={post.id}>
             <Link href={`/posts/${post.category}/${post.id}`}>
-              {post.title}
+              <h2>{post.title}</h2>
             </Link>
+            <p>Category: {post.category}</p>
+            <p>Date: {post.date}</p>
           </li>
         ))}
       </ul>
